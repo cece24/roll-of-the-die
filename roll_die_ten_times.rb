9fa6d5142add61ebede5dd6d98230218c20a27b5
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-21 18:34:05 -0400

    Remove extra code to follow instructions

--- roll_die_ten_times.rb
+++ roll_die_ten_times.rb
@@ -1,17 +1,4 @@
-ten_rolls = []
-
-includes_6 = false
-
-while includes_6 == false
+10.times do
   roll = Random.rand(1..6)
-  ten_rolls << roll
-  puts "The result of your roll is #{roll}."
-
-  if roll == 6
-    includes_6 = true
-  end
+  puts "The result of your roll is #{roll}"
 end
-
-puts "Rolls include 0: #{ten_rolls.include?(0)}"
-
-puts "Rolls include at least one 6: #{ten_rolls.include?(6)}"
